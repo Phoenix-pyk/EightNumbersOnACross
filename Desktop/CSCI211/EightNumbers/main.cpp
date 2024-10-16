2023d3e1c67e790dad0e36af1633a7486de820f3
Phoenix <161513433+FeeNixPYK@users.noreply.github.com>
2024-10-16 18:31:05 -0400

    My CODE SUCKS

--- Desktop/CSCI211/EightNumbers/main.cpp
+++ Desktop/CSCI211/EightNumbers/main.cpp
@@ -11,34 +11,47 @@ while(help[ca][i] != -1){
     i++;
 }
 return true;
-};
+}
+
+void print(int cro[]){
+    static int sol = 1;
+    cout << "Solution #" << sol++ << " : " << endl;
+    for(int i =0; i<8; i++) {
+        cout << cro[i] <<" ";
+    }
+    cout << endl;
+    cout << " " << cro[0] << cro[1] << " " << endl;
+    cout << cro[2] << cro[3] << cro[4] << cro[5] << endl;
+    cout << " " << cro[6] << cro[7] << " " << endl;
+}
 
 // putting numbers in the array
-void eightnumbers(int cro[], int ca){
+void eightnumbers(int cro[], int hel[][5], int ca){
     if (ca==8) {
         print(cro); // print the result
         return;
     }
     for (int i = 1; i<9; i++){
         cro[ca]= i; // place the number
-        if (ok(cro,ca)) eightnumbers(cro, ca+1); // if the number is valid, move onto next square.
+        if (ok(cro,hel, ca)) eightnumbers(cro, hel, ca+1); // if the number is valid, move onto next square.
     }
-};
+}
 
 
 
 int main() {
 int cross[8];
-int helper[7][5] = {
-        {-1},
-        {0,-1},
-        {0,-1},
-        {0,1,2,-1},
-        {0,1,3,-1},
-        {2,3,4,-1},
-        {3,4,5,6,-1}
+int helper[8][5] = {
+        {-1}, //0
+        {0,-1},//1
+        {0,-1},//2
+        {0,1,2,-1},//3
+        {0,1,3,-1},//4
+        {1,4,-1},//5
+        {2,3,4,-1},//6
+        {3,4,5,6,-1}//7
     };
 
-
+eightnumbers(cross, helper, 0);
     return 0;
 }
